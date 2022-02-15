da4156c50d07fa9d5f84fa5e7457e9380dec1615
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-15 16:51:33 -0500

    add and update models/index.js

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const username = require("./username");
+const friends = require("./friends");
+const email = require("./email");
+const thoughts = require("./thoughts");
+
+module.exports = { username, friends, email, thoughts };
