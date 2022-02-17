93af4173c39ee4912cf6c5921cc71cbf9a8cc2c9
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-17 17:46:57 -0500

    add indexes to routes

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const reactionsRoute = require("./reactions.route");
+const thoughtsRoute = require("./thoughts.route");
+const userRoute = require("./user.route");
+
+router.use("/reactions", reactionsRoute);
+router.use("/thoughts", thoughtsRoute);
+router.use("/user", userRoute);
+
+module.exports = router;
