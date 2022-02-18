7389ac5dd57b7ef4fe05b54ebbc6f230cba24e77
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-17 21:20:07 -0500

    Update thoughts.route.js

--- routes/api/thoughts.route.js
+++ routes/api/thoughts.route.js
@@ -2,9 +2,16 @@ const router = require("express").Router();
 const {
   getThoughts,
   createThought,
+  getSingleThought,
+  updateThought,
+  deleteThought,
 } = require("../../controllers/thoughts.controller");
 
 router.route("/").get(getThoughts).post(createThought);
-router.route("/:thoughtId");
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 module.exports = router;
