d857a40d4aad4aa2d7efcd3e66b92c77d7c49334
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-17 20:57:34 -0500

    update routes.thoughts

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
-const reactionsRoute = require("./reactions.route");
+
 const thoughtsRoute = require("./thoughts.route");
 const userRoute = require("./user.route");
 
-// router.use("/reactions", reactionsRoute);
-// router.use("/thoughts", thoughtsRoute);
+router.use("/thoughts", thoughtsRoute);
 router.use("/user", userRoute);
 
 module.exports = router;
